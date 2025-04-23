95af82e5b4fff263542be467cbbcd896b559af22
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-23 15:06:43 +0530

    [Deepak]add: initial project setup

---
+++ src/main/java/com/bridgelab/employeeWage/EmployeeWageApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelab.employeeWage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelab/employeeWage/EmployeeWageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelab.employeeWage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
