0f12d0ff3da14a63ca69281dc47e62644889db68
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-24 10:55:26 +0530

    [Deepak]refactor: add validation in EmployeePayrollDTO fields

--- src/main/java/com/bridgelab/employeeWage/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelab/employeeWage/dto/EmployeePayrollDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelab.employeeWage.dto;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,11 +12,13 @@ import lombok.Setter;
 @NoArgsConstructor
 public class EmployeePayrollDTO {
 
-    private int id;
+    @NotEmpty(message = "Employee name cannot be empty")
     private String name;
 
+    @NotEmpty(message = "Employee gender cannot be empty")
     private String gender;
 
+    @Min(value = 500, message = "Min wage should be more than 500")
     private int salary;
 
     public EmployeePayrollDTO(String name, String gender, int salary) {
