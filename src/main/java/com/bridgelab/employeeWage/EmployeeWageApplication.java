a13bd3de8572bf276586f3d5a1dda7582156078d
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-24 10:42:57 +0530

    [Deepak]add: .env added and config db on dev properties

--- src/main/java/com/bridgelab/employeeWage/EmployeeWageApplication.java
+++ src/main/java/com/bridgelab/employeeWage/EmployeeWageApplication.java
@@ -1,5 +1,6 @@
 package com.bridgelab.employeeWage;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,16 @@ public class EmployeeWageApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(EmployeeWageApplication.class, args);
+
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")  // Root directory where .env is located
+				.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		ApplicationContext Context = SpringApplication.run(EmployeeWageApplication.class, args);
 		log.info("AddressBook app is started in {} Environment", Context.getEnvironment().getProperty("environment"));
 	}
